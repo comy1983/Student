146b97a1b46bae93aac30a64ab6530c69ee189b4
comy1983 <4a.hg83@gmail.com>
2026-05-27 20:38:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,8 @@ if uploaded_file is not None:
                     with st.spinner("🔄 يقوم الذكاء الاصطناعي حالياً بقراءة البيانات وصياغة الخطط التربوية..."):
                         try:
                             genai.configure(api_key=api_key)
-                            model = genai.GenerativeModel('gemini-1.5-pro')
+                            model = genai.GenerativeModel('gemini-pro')
+
  
 
                             response = model.generate_content(prompt)
